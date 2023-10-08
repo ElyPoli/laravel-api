5f8e16b1878343d9de373520c428e36450529786
ElyPoli <itsos.poli@gmail.com>
2023-10-08 10:52:10 +0200

    added projects route and model

---
+++ app/Models/Project.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    // Specifico in che tipologia convertire i dati di alcune colonne per facilitarne la lettura
+    protected $casts = [
+        "tools_used" => "array"
+    ];
+
+    // Creo un array con indicate le colonne da popolare
+    protected $fillable = [
+        "title",
+        "description",
+        "tools_used",
+        "repository_link",
+        "url",
+        "slug"
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,10 +23,28 @@ Route::get('/admin', function () {
     return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Raggruppo le rotte per la parte admin di "projects" e applicco il middleware per verificare che l'utente sia loggato
+Route::middleware(['auth', 'verified'])
+    ->prefix("admin") // aggiunge a tutte le rotte il prefisso indicato
+    ->name("admin.") // aggiunge al name di tutte le rotte il prefisso indicato
+    ->group(function () {
+        // Create
+        Route::get("/projects/create", [ProjectController::class, "create"])->name("projects.create");
+        Route::post("/projects", [ProjectController::class, "store"])->name("projects.store");
+        // Read
+        Route::get("/projects", [ProjectController::class, "index"])->name("projects.index");
+        Route::get("/projects/{project}", [ProjectController::class, "show"])->name("projects.show");
+        // Udate
+        Route::get("/projects/{project}/edit", [ProjectController::class, "edit"])->name("projects.edit");
+        Route::put("/projects/{project}", [ProjectController::class, "update"])->name("projects.update");
+        // Destroy
+        Route::delete("/projects/{project}", [ProjectController::class, "destroy"])->name("projects.destroy");
+    });
+
 Route::middleware('auth')->group(function () {
     Route::get('/admin/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
     Route::patch('/admin/profile', [ProfileController::class, 'update'])->name('admin.profile.update');
     Route::delete('/admin/profile', [ProfileController::class, 'destroy'])->name('admin.profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
