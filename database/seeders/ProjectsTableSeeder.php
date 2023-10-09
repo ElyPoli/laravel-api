b20e60b0608c92350826efe77bd8ac337bd9d1dc
ElyPoli <itsos.poli@gmail.com>
2023-10-09 11:04:32 +0200

    update projects seeder

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -17,7 +17,7 @@ class ProjectsTableSeeder extends Seeder
     public function run(Faker $faker): void
     {
         // Elimino i dati presenti nella tabella
-        Project::truncate();
+        \App\Models\Project::truncate();
 
         // Genero dei dati provvisori con il faker
         for ($i = 0; $i < 10; $i++) {
@@ -25,7 +25,7 @@ class ProjectsTableSeeder extends Seeder
 
             $project->title = $faker->word();
             $project->description = $faker->text(100);
-            $project->tools_used = $faker->sentences(2);
+            $project->tools_used = $faker->words(5);
             $project->repository_link = $faker->url();
             $project->url = $faker->url();
             $project->slug = Str::slug($project["title"]);
