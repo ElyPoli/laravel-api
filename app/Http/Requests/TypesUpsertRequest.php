c0c035599e6f0b5bc22547bccdea71f2084a93d5
ElyPoli <itsos.poli@gmail.com>
2023-10-11 22:31:15 +0200

    added form request for types

---
+++ app/Http/Requests/TypesUpsertRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TypesUpsertRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "required|string|max:100",
+            "color" => "required|string|max:100",
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Il campo nome è obbligatorio.',
+            'name.max' => 'Il nome non può superare i :max caratteri.',
+            'color.required' => 'Il campo colore è obbligatorio.',
+        ];
+    }
+}
