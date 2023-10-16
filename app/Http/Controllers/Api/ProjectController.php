4f7e820140a43261e98a44db9a301ea0af4ed99a
ElyPoli <itsos.poli@gmail.com>
2023-10-16 15:29:26 +0200

    update function projects api (add eager loading)

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,8 +11,8 @@ class ProjectController extends Controller
     // Ritorna un file json con l'elenco di tutti i dati all’interno della tabella del db
     public function index()
     {
-        // Recupero una lista di progetti dal database e li suddivido in pagine
-        $projects = Project::paginate(5);
+        // Recupero una lista di progetti dal database, li suddivido in pagine e aggiungo i dati delle relazioni "type" e "technologies"
+        $projects = Project::with(["type", "technologies"])->paginate(5);
 
         // Ritorno dei dati sotto forma di un file json
         return response()->json([
